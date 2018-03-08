961efc46171dc787f1e493afed409721014186fd
sb464f <sb464f@att.com>
2018-03-08 00:22:04 +0000

    Update UCP versions and remove duplicates
    Change-Id: Id0af5dfaca00d5ed42e5a66aa4b9e9a32046200a

--- region/atl-lab1/bootstrap/genesis/genesis-user-data.sh
+++ region/atl-lab1/bootstrap/genesis/genesis-user-data.sh
@@ -7,7 +7,20 @@ apt-get install -y default-jre-headless ntpdate
 echo '10.24.20.100 genesis' >> /etc/hosts
 echo genesis >> /etc/hostname
 hostname genesis
+update_etc_interfaces() {
+cat << EOF | sudo tee -a /etc/network/interfaces
+iface ens3 inet dhcp
+iface ens4 inet dhcp
+iface ens5 inet dhcp
+iface ens6 inet dhcp
+iface ens7 inet dhcp
+iface ens8 inet dhcp
+EOF
+}
 
-sudo  curl -L --insecure -o /usr/local/share/ca-certificates/gd_bundle-g2.crt https://certs.godaddy.com/repository/gd_bundle-g2.crt
-sudo update-ca-certificates
+sudo ifup ens4
+sudo ifup ens5
+sudo ifup ens6
+sudo ifup ens7
+sudo ifup ens8
 
