0caa7dd7b5fb7f1cb0c56595d13f1f25d833904f
Roman Gorshunov <roman.gorshunov@att.com>
2018-04-16 11:56:07 +0200

    GoDaddy intermediate certs no longer needed
    Since artifactory repo has full certificates chain now, we don't need to
    download intermediate certificated from GoDaddy.
    
    Change-Id: Ib3da9039d7a01b38d7afb4736de3bef069473bdc

--- region/atl-lab1/bootstrap/prom-build/prom-build-user-data.sh
+++ region/atl-lab1/bootstrap/prom-build/prom-build-user-data.sh
@@ -6,9 +6,3 @@ apt-get install -y docker.io curl ipmitool make
 adduser ubuntu docker
 
 apt-get install -y default-jre-headless
-
-
-curl -L --insecure -o /usr/local/share/ca-certificates/gd_bundle-g2.crt https://certs.godaddy.com/repository/gd_bundle-g2.crt
-update-ca-certificates
-
-apt-get install -y default-jre-headless
